7b63495ec133d04a82b99d4b6f8e796d482e5bcc
platylm <n_nareenart@kkumail.com>
2020-03-12 16:31:11 +0700

    [Created]: file main.go

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,6 +13,12 @@ func main() {
 		c.JSON(200, gin.H{
 			"message": "hello world",
 		})
+	})
+	r.GET("/welcome", func(c *gin.Context) {
+		firstname := c.DefaultQuery("firstname", "Guest")
+		lastname := c.Query("lastname")
+
+		c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
 	})
 	log.Fatal(r.Run(":3000"))
 }
